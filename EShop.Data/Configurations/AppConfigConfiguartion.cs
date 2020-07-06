bfb09d09cc5cdd450589932b4715f15bf77802bb
hieubui97 <hieubuim012@gmail.com>
2020-07-06 22:40:22 +0700

    Migration Database

---
+++ EShop.Data/Configurations/AppConfigConfiguartion.cs
@@ -0,0 +1,20 @@
+﻿using EShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Data.Configurations
+{
+    class AppConfigConfiguartion : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            builder.ToTable("AppConfigs");
+            builder.HasKey(x => x.Key);
+            builder.Property(x => x.Value).IsRequired(true);
+        }
+    }
+}
---
+++ EShop.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Data.Configurations
+{
+    public class ProductInCategoryConfiguration : IEntityTypeConfiguration<ProductInCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductInCategory> builder)
+        {
+            builder.ToTable("ProductInCategory");
+
+            builder.HasKey(x => new { x.ProductId, x.CategoryId });
+
+            builder.HasOne(x => x.Product).WithMany(pc => pc.ProductInCategories).HasForeignKey(pc => pc.ProductId);
+
+            builder.HasOne(x => x.Category).WithMany(pc => pc.ProductInCategories).HasForeignKey(pc => pc.CategoryId);
+        }
+    }
+}
